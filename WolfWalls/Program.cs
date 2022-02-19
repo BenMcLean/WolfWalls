7584511f201bf9e32829a97ef236069089a24412
Benjamin McLean <mclean.ben@gmail.com>
2022-02-18 20:58:35 -0600

    Make animated GIF

--- WolfWalls/Program.cs
+++ WolfWalls/Program.cs
@@ -1,5 +1,5 @@
-﻿using SixLabors.ImageSharp;
-using System;
+﻿using AnimatedGif;
+using SixLabors.ImageSharp;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,12 +16,19 @@ namespace WolfWalls
 				throw new InvalidDataException();
 			bool[][] map = LoadMap(args[0]);
 			byte[] image = new byte[map.Length * map.Length * 4];
+			SixLabors.ImageSharp.Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(image, map.Length, map.Length)
+				.SaveAsPng("frame0.png");
 			for (int x = 0; x < map.Length; x++)
 				for (int y = 0; y < map[x].Length; y++)
 					if (!map[x][y])
 						image.DrawPixel(140, 140, 140, 255, x, y, map.Length);
-			Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(image, map.Length, map.Length)
-				.SaveAsPng("output.png");
+			SixLabors.ImageSharp.Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(image, map.Length, map.Length)
+				.SaveAsPng("frame1.png");
+			using (AnimatedGifCreator gif = AnimatedGif.AnimatedGif.Create("output.gif", 256))
+			{
+				gif.AddFrame(System.Drawing.Image.FromFile("frame0.png"), delay: -1, quality: GifQuality.Bit8);
+				gif.AddFrame(System.Drawing.Image.FromFile("frame1.png"), delay: -1, quality: GifQuality.Bit8);
+			}
 		}
 		public static bool[][] LoadMap(string path)
 		{
